7f5484a9f30fcea88722264e59243ebe558c4530
Shocky20 <131475128+Shocky20@users.noreply.github.com>
2025-06-18 02:20:15 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello from webhook brother123456');
+  res.end('ALHAMDULILLAH');
 });
 
 server.listen(3001, () => {
