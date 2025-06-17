c594e62ddd8ab6d6ec469de30303fe94eae62fbb
Shocky20 <131475128+Shocky20@users.noreply.github.com>
2025-06-18 03:02:26 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end("wow");
+  res.end("kon bola kon bola");
 });
 
 server.listen(3001, () => {
