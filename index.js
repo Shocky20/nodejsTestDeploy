43362d2da7e453a0838a31e4051d0f33d33d5e00
Shocky20 <131475128+Shocky20@users.noreply.github.com>
2025-06-18 11:50:35 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end("alahmdulillah all praises to allah");
+  res.end("ALHAMDULILLAH");
 });
 
 server.listen(3001, () => {
